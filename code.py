0f871fd49640da319c98a95076a906109439f938
Himanshu809f <garg38223@gmail.com>
2026-02-04 11:13:42 +0530

    python file update

---
+++ code.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+data={"NAME":["Alice","Bob","Charlie"],
+      "AGE":[24,27,22],
+      "CITY":["New York","Los Angeles","Chicago"]}
+
+df = pd.DataFrame(data)
+
+data_dir='data'
+os.makedirs(data_dir, exist_ok=True)
+
+
+file_path = os.path.join(data_dir, 'data.csv')
+df.to_csv(file_path, index=False)
+print(f"CSV saved to {file_path}")
